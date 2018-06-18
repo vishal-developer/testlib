a69b9930fc4a9424cd3b96292529d213de04ba1b
vishal chougule <vishal.chougule@pubmatic.com>
2018-06-18 22:56:24 +0530

    initial commit

---
+++ src/main/java/com/pro/lib/Pojo.java
@@ -0,0 +1,16 @@
+package com.pro.lib;
+
+public class Pojo {
+
+    private String name;
+
+   public Pojo(String name){
+       this.name = name;
+   }
+
+    public String getName() {
+        return name;
+    }
+
+
+}
